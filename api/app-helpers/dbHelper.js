ac14946b534f54e59c953d44ae48ee68e7855c80
DESKTOP-P148DK7\VuQuang <vuquangkhtn@gmail.com>
2017-11-28 10:48:12 +0700

    change uri
    change uri

--- api/app-helpers/dbHelper.js
+++ api/app-helpers/dbHelper.js
@@ -1,5 +1,8 @@
 var mongoose = require("mongoose");
 
-mongoose.connect('mongodb://1412430:1234567890@ds119306.mlab.com:19306/btcn04');
+// mongoose.connect('mongodb://1412430:1234567890@ds119306.mlab.com:19306/btcn04');
 
-// mongoose.connect('localhost:27017/qlgiaodich');
+mongoose.connect(process.env.MONGOLAB_URI, options, function(error) {
+  // Check error in initial connection. There is no 2nd param to the callback.
+  console.log(error);
+});
