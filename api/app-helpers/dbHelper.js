93104c7ba33b0cc6469cfe464c51f5ccc1a33f48
DESKTOP-P148DK7\VuQuang <vuquangkhtn@gmail.com>
2017-11-28 10:55:10 +0700

    fix url
    fix url

--- api/app-helpers/dbHelper.js
+++ api/app-helpers/dbHelper.js
@@ -2,7 +2,4 @@ var mongoose = require("mongoose");
 
 // mongoose.connect('mongodb://1412430:1234567890@ds119306.mlab.com:19306/btcn04');
 
-mongoose.connect(process.env.MONGOLAB_URI, options, function(error) {
-  // Check error in initial connection. There is no 2nd param to the callback.
-  console.log(error);
-});
+mongoose.connect(process.env.MONGOLAB_URI);
